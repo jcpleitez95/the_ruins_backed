7eb6eb17336660657bde997652920c0cf78bed5e
jcpleitez95 <jcpleitez@outlook.com>
2021-02-05 14:26:30 -0500

    added map deed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Map.destroy_all 
+
+Map.create(map_image: "https://ujli6a.dm.files.1drv.com/y4p-ymofG69bEkZc6rYJ3O3ZEFQ0rSvt9w17APxaabo0JHxIMZu0jQPRAB86WMwfQ7vW0S01oNkuMB_BZQZQNkDX-ZNhRcygccMCezoRACUd3YiAHQ-lilnDlK8gqDUs1CCbfrgNYmRoLOIpwnzLh9S-bWI6gW0Bq7wVcK-lQRp8oCdycxp_fhcSmRwjjP3Nr1zeaw_3lsadWUD7Mc5BL9arPiv7ddOixobB-unEOxprEk/map.png?psid=1")
